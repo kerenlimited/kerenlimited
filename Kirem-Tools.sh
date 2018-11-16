6efb0a49890f611aff5b95bfc2adac0ce5165479
kerenlimited <45083353+kerenlimited@users.noreply.github.com>
2018-11-16 10:11:17 +0700

    https://github.com/kerenlimited/kerenlimited.git
    Selamat mencoba
    #HAPPY FUN :)
    GOODBYE!

---
+++ Kirem-Tools.sh
@@ -0,0 +1,14 @@
+#!/data/data/com.termux
+
+sleep 2
+figlet KEREN LIMITED
+sleep 2
+pkg install zip
+sleep2
+pkg install unzip
+sleep 1
+figlet GoodBye!
+figlet naik kereta dulu sana :v
+pkg install sl
+sl
+unzip kerenlimited.zip
---
+++ README.txt
@@ -0,0 +1,13 @@
+SALAM DARI ØSC4R O'4S1S
+NEW TOOLS
+-ALL IN 1 TOOLS
+
+INSTALLED:
+pkg install update
+pkg install upgrade
+pkg install zip
+pkg install bash
+pkg install unzip
+
+#HAPPY FUN :)
+GOODBYE!
