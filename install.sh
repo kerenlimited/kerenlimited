d1f8143628f62afcaf4620a568063dda7a363339
kerenlimited <45083353+kerenlimited@users.noreply.github.com>
2018-11-17 14:13:51 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,72 @@
+pkg install update
+pkg install upgrade
+pkg install figlet
+pkg install zip
+figlet WELCOME
+figlet EKSTRAK TOOLS
+figlet 1000 in 1  TOOLS
+sleep 5
+clear
+figlet Ekstrak tools
+figlet mungkin akan lama
+sleep 3
+figlet mohon bersabar
+sleep 4
+clear
+pkg install unzip
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+unzip tools0.zip
+clear
+
